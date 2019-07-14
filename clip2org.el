779f3c30c8fce3885555713de8e0d412a643afff
Yiming Chen <dsdshcym@gmail.com>
2019-07-14 08:29:06 +0800

    refactor: extract temporary variable `hour`

--- clip2org.el
+++ clip2org.el
@@ -246,17 +246,18 @@ Returns nil if there is no data for last run."
   "Parse datetime for a clipping. The format is like this:
 - Monday, July 8, 2019 11:15:29 PM
 - Tuesday, July 9, 2019 6:48:34 AM"
-  (let ((parsed-datetime (parse-time-string datetime)))
+  (let* ((parsed-datetime (parse-time-string datetime))
+        (hour (nth 2 parsed-datetime)))
     (setf (nth 2 parsed-datetime)
       (cond
-        ((and (< (nth 2 parsed-datetime) 12) (string-match "PM$" datetime))
-          (+ (nth 2 parsed-datetime) 12))
+        ((and (< hour 12) (string-match "PM$" datetime))
+          (+ hour 12))
 
-        ((and (eq (nth 2 parsed-datetime) 12) (string-match "AM$" datetime))
+        ((and (eq hour 12) (string-match "AM$" datetime))
           0)
 
         (t
-          (nth 2 parsed-datetime))))
+          hour)))
     (encode-time parsed-datetime)))
 
 (defun clip2org (&optional all clipping-file)
